badfbff8e915da52fe6feb4e1fbee2bb362759cc
wakfi <55608093+wakfi@users.noreply.github.com>
2020-06-04 15:31:43 -0700

    Add option to only return element one after flag

--- src/util/parsePositionalArgs.js
+++ src/util/parsePositionalArgs.js
@@ -1,7 +1,10 @@
 const dashflagRegex = /(?<=\s|^)-[a-zA-Z]+(?=\s|$)/;
 
-function parsePositionalArgs(args,keys,flags,flagRegex) 
+function parsePositionalArgs(args,keys,flags,options) 
 {
+	if(typeof options === 'undefined') options = {};
+	let flagRegex = options.flagRegex;
+	let singlePosition = options.singlePosition;
 	if(typeof flagRegex === 'undefined') flagRegex = dashflagRegex;
 	if(!(flagRegex instanceof RegExp)) throw new TypeError(`flagRegex must be a Regular Expression`);
 	if(keys.length != flags.length) throw new RangeError(`Must same number of keys and flags`);
@@ -13,12 +16,13 @@ function parsePositionalArgs(args,keys,flags,flagRegex)
 		{
 			const key = keys[i];
 			const indexKey = args.indexOf(flag);
-			const nextFlagIndex = args.slice(indexKey+1).findIndex(arg => flagRegex.test(arg));
+			const nextFlagIndex = singlePosition ? 1 : args.slice(indexKey+1).findIndex(arg => flagRegex.test(arg));
 			const val = args.splice(indexKey+1, nextFlagIndex==-1 ? args.length : nextFlagIndex).join(' ');
 			Object.defineProperty(obj, key, {value: val, writable: true, enumerable: true, configurable: true});
 			args.splice(indexKey,1);
 		}
 	}
+	Object.defineProperty(obj, 'args', {value: args, writable: true, enumerable: true, configurable: true});
 	return obj;
 }
 
