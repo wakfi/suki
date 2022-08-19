28be2f7206731a5f080c7e679857ba3fd4c7d0d4
Walker Gray <55608093+wakfi@users.noreply.github.com>
2022-08-20 00:26:35 +0200

    Update deps

--- src/commands/general/info.js
+++ src/commands/general/info.js
@@ -1,33 +1,61 @@
-const got = require('got');
-const path = require('path');
-const MessageEmbed = require((require.resolve('discord.js')).split(path.sep).slice(0, -1).join(path.sep) + `${path.sep}structures${path.sep}MessageEmbed.js`);
+const { default: fetch } = require("node-fetch");
+const path = require("path");
+const MessageEmbed = require(require
+  .resolve("discord.js")
+  .split(path.sep)
+  .slice(0, -1)
+  .join(path.sep) + `${path.sep}structures${path.sep}MessageEmbed.js`);
 const authorReply = require(`${process.cwd()}/util/authorReply.js`);
 const { prefix } = require(`${process.cwd()}/components/config.json`);
 
 module.exports = {
-	name: 'info',
-	description: 'Information about the bot and its development',
-	category: 'general',
-	permLevel: 'User',
-	noArgs: true,
-	async execute(message, args) {
-		try {
-			const botVersion = (await got('https://github.com/wakfi/suki/releases')).body.split('/wakfi/suki/releases/tag/')[1].split('"')[0];
-			const djsVersion = (await got('https://github.com/wakfi/suki/blob/master/package.json', {resolveBodyOnly: true})).split('>discord.js<')[1].split('^')[1].split('<')[0];
-			const embed = new MessageEmbed()
-				.setTitle(`Suki`)
-				.setThumbnail(message.client.user.displayAvatarURL())
-				.setDescription(`This bot was created by <@193160566334947340> for the Clue Crew Server`)
-				.addField(`Prefix`,`Use ${prefix} or ${message.client.user} to invoke commands`)
-				.addField(`Help commands`,`${prefix}help, ${prefix}commands, ${prefix}command, ${prefix}?`)
-				.addField(`Library`,`Created in JavaScript using [discord.js](https://discord.js.org/) v${djsVersion}, a powerful node.js module that allows you to interact with the Discord API very easily`)
-				.addField(`Repository`,`This software is licensed under the MIT license. The GitHub repository for this project can be found at: https://github.com/wakfi/suki`)
-				.setFooter(`suki ${botVersion}`)
-				.setTimestamp(new Date())
-				.setColor(0xFF00FF);
-			authorReply(message, embed);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+  name: "info",
+  description: "Information about the bot and its development",
+  category: "general",
+  permLevel: "User",
+  noArgs: true,
+  async execute(message, args) {
+    try {
+      const botVersion = await fetch("https://github.com/wakfi/suki/releases")
+        .then((r) => r.text())
+        .then(
+          (body) => body.split("/wakfi/suki/releases/tag/")[1].split('"')[0]
+        );
+      const djsVersion = await fetch(
+        "https://github.com/wakfi/suki/blob/master/package.json"
+      )
+        .then((r) => r.text())
+        .then(
+          (body) => body.split(">discord.js<")[1].split("^")[1].split("<")[0]
+        );
+      const embed = new MessageEmbed()
+        .setTitle(`Suki`)
+        .setThumbnail(message.client.user.displayAvatarURL())
+        .setDescription(
+          `This bot was created by <@193160566334947340> for the Clue Crew Server`
+        )
+        .addField(
+          `Prefix`,
+          `Use ${prefix} or ${message.client.user} to invoke commands`
+        )
+        .addField(
+          `Help commands`,
+          `${prefix}help, ${prefix}commands, ${prefix}command, ${prefix}?`
+        )
+        .addField(
+          `Library`,
+          `Created in JavaScript using [discord.js](https://discord.js.org/) v${djsVersion}, a powerful node.js module that allows you to interact with the Discord API very easily`
+        )
+        .addField(
+          `Repository`,
+          `This software is licensed under the MIT license. The GitHub repository for this project can be found at: https://github.com/wakfi/suki`
+        )
+        .setFooter(`suki ${botVersion}`)
+        .setTimestamp(new Date())
+        .setColor(0xff00ff);
+      authorReply(message, embed);
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
--- src/commands/image/hugemoji.js
+++ src/commands/image/hugemoji.js
@@ -1,9 +1,9 @@
-const got = require('got');
-const rp = (query) => got(query, {resolveBodyOnly: true}); //originally use request-promise, now deprecated. This lambda is for backwards compatability
-const emojiUnicode = require('emoji-unicode');
-var svgToPng = require('svg-to-png');
-var path = require('path');
-var fs = require('fs-extra');
+const { default: fetch } = require("node-fetch");
+const rp = (query) => fetch(query).then((r) => r.text()); //originally use request-promise, now deprecated. This lambda is for backwards compatability
+const emojiUnicode = require("emoji-unicode");
+var svgToPng = require("svg-to-png");
+var path = require("path");
+var fs = require("fs-extra");
 
 /* license for emojilib.json adapted from another source
 The MIT License (MIT)
@@ -31,86 +31,121 @@ SOFTWARE.
 const emojiMap = require(`${process.cwd()}/components/emojilib.json`);
 
 module.exports = {
-	name: 'hugemoji',
-	description: 'create a real big version of an emoji',
-	category: 'image',
-	usage: ['<emoji>'],
-	aliases: ['hugeemoji'],
-	permLevel: 'User',
-	args: true,
-	async execute(message, args) {
-		const messageElement = args[0];
-		if(messageElement.includes(`>`) && messageElement.includes(`:`))
-		{
-			//emoji is a custom server emoji
-			const discordEmojisUri = `https://cdn.discordapp.com/emojis/`;
-			const splitEmoji = messageElement.split(`:`);
-			const fileType = splitEmoji.shift() === `<a` ? `.gif` : `.png`; //animated or image
-			const emojiName = splitEmoji.shift();
-			const emojiSnowflake = splitEmoji.shift().split(`>`)[0];
-			const emojiImageUrl = `${discordEmojisUri}${emojiSnowflake}${fileType}`;
-			message.channel.send({files: 
-				[{attachment: emojiImageUrl,
-				name: `${emojiName}${fileType}`}]
-			})
-			.catch(err=>{console.error(`Error sending a message:\n\t${typeof err==='string'?err.split('\n').join('\n\t'):err.stack}`)});
-		} else if(!messageElement.includes(`>`)) {
-			//text is a string
-			const twemojiDomain = `https://github.com/twitter/twemoji/blob/master/assets/svg/`;
-			const emojiToVerify = messageElement;
-			const emojiInUnicode = emojiUnicode(emojiToVerify).split(' ').join('-');
-			const svgDomain = `${twemojiDomain}${emojiInUnicode}.svg`;
-			let githubResponseA = null;
-			try {
-				//we need to verify that its an emoji
-				githubResponseA = await rp(svgDomain);
-			} catch(err) {
-				//there are some emojis that have slight disconnections between their codepoints and their url, so try to fix
-				try {
-					const svgSecondDomain = `${twemojiDomain}${emojiInUnicode.slice(0,emojiInUnicode.lastIndexOf('-'))}.svg`;
-					githubResponseA = await rp(svgSecondDomain);
-				} catch(moreErr) {
-					//the number/digit emojis have the 'fe0f' codepoint in the middle but their twemoji urls don't for some reason
-					try {
-						const svgThirdDomain = `${twemojiDomain}${emojiInUnicode.split('fe0f-').join('')}.svg`;
-						githubResponseA = await rp(svgThirdDomain);
-					} catch(stillErr) {
-						//not an emoji. the conditional is checking if its throwing a real error or just 404 not found
-						if(!JSON.stringify(e).includes(`Response code 404 (Not Found)`))
-						{
-							console.error(e.stack);
-						}
-					}
-				}
-			}
-			//this is a syntax trick to quickly see if one of the attempts succeeded before continueing
-			if(githubResponseA) 
-			{
-				//emoji is a unicode emoji 
-				const githubResponseB = await rp(githubResponseA.split(`<iframe class="render-viewer " src="`)[1].split('"')[0]);
-				//the order here is: get svg image from remote (save local), convert to png (save local), send png, delete local svg and png
-				const emojiName = emojiMap[messageElement][0] || emojiInUnicode;
-				const picFolder = `${process.cwd()}/file_dump`;
-				await fs.ensureDir(picFolder).catch(e=>{return console.error(e.stack)});
-				//data for vector image of emoji
-				const emojiSvg = await rp(githubResponseB.split('data-image  = "')[1].split('"')[0]);
-				await fs.outputFile(`${picFolder}/${emojiInUnicode}.svg`,emojiSvg);
-				//convert from svg to png
-				await svgToPng.convert(path.join(picFolder,`${emojiInUnicode}.svg`), picFolder, {defaultWidth:722,defaultHeight:722},{type:"image/png"});
-				await message.channel.send({files: 
-					[{attachment: `${picFolder}/${emojiInUnicode}.png`,
-					name: `${emojiName}.png`}]
-				}).catch(err=>{console.error(`Error sending a message:\n\t${typeof err==='string'?err.split('\n').join('\n\t'):err.stack}`)});
-				//cleanup created files
-				await fs.remove(`${picFolder}/${emojiInUnicode}.svg`)
-				.catch(err => {
-					console.error(err.stack)
-				});
-				await fs.remove(`${picFolder}/${emojiInUnicode}.png`)
-				.catch(err => {
-					console.error(err.stack)
-				});
-			}
-		}
-	}
+  name: "hugemoji",
+  description: "create a real big version of an emoji",
+  category: "image",
+  usage: ["<emoji>"],
+  aliases: ["hugeemoji"],
+  permLevel: "User",
+  args: true,
+  async execute(message, args) {
+    const messageElement = args[0];
+    if (messageElement.includes(`>`) && messageElement.includes(`:`)) {
+      //emoji is a custom server emoji
+      const discordEmojisUri = `https://cdn.discordapp.com/emojis/`;
+      const splitEmoji = messageElement.split(`:`);
+      const fileType = splitEmoji.shift() === `<a` ? `.gif` : `.png`; //animated or image
+      const emojiName = splitEmoji.shift();
+      const emojiSnowflake = splitEmoji.shift().split(`>`)[0];
+      const emojiImageUrl = `${discordEmojisUri}${emojiSnowflake}${fileType}`;
+      message.channel
+        .send({
+          files: [
+            { attachment: emojiImageUrl, name: `${emojiName}${fileType}` },
+          ],
+        })
+        .catch((err) => {
+          console.error(
+            `Error sending a message:\n\t${
+              typeof err === "string" ? err.split("\n").join("\n\t") : err.stack
+            }`
+          );
+        });
+    } else if (!messageElement.includes(`>`)) {
+      //text is a string
+      const twemojiDomain = `https://github.com/twitter/twemoji/blob/master/assets/svg/`;
+      const emojiToVerify = messageElement;
+      const emojiInUnicode = emojiUnicode(emojiToVerify).split(" ").join("-");
+      const svgDomain = `${twemojiDomain}${emojiInUnicode}.svg`;
+      let githubResponseA = null;
+      try {
+        //we need to verify that its an emoji
+        githubResponseA = await rp(svgDomain);
+      } catch (err) {
+        //there are some emojis that have slight disconnections between their codepoints and their url, so try to fix
+        try {
+          const svgSecondDomain = `${twemojiDomain}${emojiInUnicode.slice(
+            0,
+            emojiInUnicode.lastIndexOf("-")
+          )}.svg`;
+          githubResponseA = await rp(svgSecondDomain);
+        } catch (moreErr) {
+          //the number/digit emojis have the 'fe0f' codepoint in the middle but their twemoji urls don't for some reason
+          try {
+            const svgThirdDomain = `${twemojiDomain}${emojiInUnicode
+              .split("fe0f-")
+              .join("")}.svg`;
+            githubResponseA = await rp(svgThirdDomain);
+          } catch (stillErr) {
+            //not an emoji. the conditional is checking if its throwing a real error or just 404 not found
+            if (!JSON.stringify(e).includes(`Response code 404 (Not Found)`)) {
+              console.error(e.stack);
+            }
+          }
+        }
+      }
+      //this is a syntax trick to quickly see if one of the attempts succeeded before continueing
+      if (githubResponseA) {
+        //emoji is a unicode emoji
+        const githubResponseB = await rp(
+          githubResponseA
+            .split(`<iframe class="render-viewer " src="`)[1]
+            .split('"')[0]
+        );
+        //the order here is: get svg image from remote (save local), convert to png (save local), send png, delete local svg and png
+        const emojiName = emojiMap[messageElement][0] || emojiInUnicode;
+        const picFolder = `${process.cwd()}/file_dump`;
+        await fs.ensureDir(picFolder).catch((e) => {
+          return console.error(e.stack);
+        });
+        //data for vector image of emoji
+        const emojiSvg = await rp(
+          githubResponseB.split('data-image  = "')[1].split('"')[0]
+        );
+        await fs.outputFile(`${picFolder}/${emojiInUnicode}.svg`, emojiSvg);
+        //convert from svg to png
+        await svgToPng.convert(
+          path.join(picFolder, `${emojiInUnicode}.svg`),
+          picFolder,
+          { defaultWidth: 722, defaultHeight: 722 },
+          { type: "image/png" }
+        );
+        await message.channel
+          .send({
+            files: [
+              {
+                attachment: `${picFolder}/${emojiInUnicode}.png`,
+                name: `${emojiName}.png`,
+              },
+            ],
+          })
+          .catch((err) => {
+            console.error(
+              `Error sending a message:\n\t${
+                typeof err === "string"
+                  ? err.split("\n").join("\n\t")
+                  : err.stack
+              }`
+            );
+          });
+        //cleanup created files
+        await fs.remove(`${picFolder}/${emojiInUnicode}.svg`).catch((err) => {
+          console.error(err.stack);
+        });
+        await fs.remove(`${picFolder}/${emojiInUnicode}.png`).catch((err) => {
+          console.error(err.stack);
+        });
+      }
+    }
+  },
 };
