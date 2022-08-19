36aa3c066fac9f8ee8085d60b33a723f81d032a0
Walker Gray <55608093+wakfi@users.noreply.github.com>
2022-08-20 01:07:47 +0200

    Delete ancient code

--- src/util/replaceLast.js
+++
@@ -1,9 +0,0 @@
-String.prototype.reverse = function() {
-    return this.split('').reverse().join('');
-};
-
-function replaceLast(string, substring, replacement) {
-    return string.reverse().replace(new RegExp(substring.reverse()), replacement.reverse()).reverse();
-};
-
-module.exports = replaceLast;
--- src/util/resolveMention.js
+++
@@ -1,8 +0,0 @@
-function resolveMention(string, pattern)
-{
-	if(!(pattern instanceof RegExp)) throw new TypeError('pattern must be a regular expression');
-	if(!string) return null;
-	return pattern.test(string) ? pattern.exec(string)[1] : string;
-}
-
-module.exports = resolveMention;
